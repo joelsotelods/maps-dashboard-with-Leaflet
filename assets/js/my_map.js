5d7b9cac2de65caba7d67ed60d990766f7f82261
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-05-18 13:22:03 -0500

    Added the code for the map

--- assets/js/my_map.js
+++ assets/js/my_map.js
@@ -1,9 +1,18 @@
+
+
 var quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-console.log(quakes)
 var plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+console.log(quakes)
 console.log(plates)
 
 console.log(API_KEY)
+
+function markerSize(magnitude) {
+    return magnitude * 5;
+};
+
+var earthquakes = new L.LayerGroup();
 
 d3.json(quakes, function(geoJSON) {
     L.geoJSON(geoJSON.features, {
@@ -14,7 +23,7 @@ d3.json(quakes, function(geoJSON) {
 
         style: function(geoFeature) {
             return {
-                fillColor: Color(geoFeature.properties.mag),
+                fillColor: 'purple',//Color(geoFeature.properties.mag),
                 fillOpacity: 0.5,
                 weight: 0.1,
                 color: 'black'
@@ -22,9 +31,55 @@ d3.json(quakes, function(geoJSON) {
         },
 
         onEachFeature: function(feature, layer) {
-            layer.bindPopup(
-                "<h4 style='text-align:center;'>" + new Date(feature.properties.time) +
-                "</h4> <hr> <h5 style='text-align:center;'>" + feature.properties.title + "</h5>");
+            layer.bindPopup("<h4 style='text-align:center;'>" + new Date(feature.properties.time) + "</h4> <hr> <h5 style='text-align:center;'>" + feature.properties.title + "</h5>");
         }
-    });
+    })
+    .addTo(earthquakes);
+    createMap(earthquakes);
 });
+
+var boundary = new L.LayerGroup();
+
+function createMap() {
+
+    var streetMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: API_KEY
+    });
+
+    var lightMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.light',
+        accessToken: API_KEY
+    });
+
+    var satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: API_KEY
+    });
+
+    var baseLayers = {
+        "Street": streetMap,
+        "Light": lightMap,
+        "Satellite": satellite
+    };
+
+    var overlays = {
+        "Earthquakes": earthquakes,
+        "Plate Boundaries": boundary
+    };
+
+    var mymap = L.map('mymap', {
+        center: [40, -110],
+        zoom: 5,
+        layers: [streetMap, earthquakes, boundary]
+    });
+
+    L.control.layers(baseLayers, overlays).addTo(mymap);
+
+}
